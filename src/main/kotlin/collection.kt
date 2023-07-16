5a2937f98a709094ab9cdec7a35a6fc5c9bb8bda
Abhishek <username>
2023-07-17 01:01:11 +0545

    Basic kotlin code

---
+++ src/main/kotlin/collection.kt
@@ -0,0 +1,19 @@
+fun main(){
+    val x= listOf(
+        "hello",
+        "this",
+        "is",
+        "list",
+
+
+    )
+//Traversal of  list
+    var a=10;
+    for (item in x){
+        println(item)
+    }
+    do{
+        a--;
+        println("do nothing just chill")
+    }while(a>1)
+}
